f66c19bdbdf4f570f36f2fcb7d994f0ac4fb7fb1
sylvain cherrier <scherrier@ncelrnd0045.rnd.amadeus.net>
2016-08-08 14:15:52 +0200

    Appengine blog project: add DB handling

--- appengine_blog/main.py
+++ appengine_blog/main.py
@@ -1,11 +1,19 @@
 import os
-
 import webapp2
 import jinja2
+from google.appengine.ext import db
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
+
+
+# DB definition
+class BlogEntry(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+
 
 class BaseHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -18,21 +26,47 @@ class BaseHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
 class BlogFrontPage(BaseHandler):
     def get(self):
-        self.render('front_page.html', posts=[])
+        # Run query to retrieve posts
+        posts = db.GqlQuery("select * from BlogEntry")
+        self.render('front_page.html', posts=posts)
+
 
 class BlogNewPost(BaseHandler):
+    def render_new_post(self, subject='', content='', error=''):
+        self.render('new_post.html',
+                    subject=subject,
+                    content=content,
+                    error=error)
+
     def get(self):
-        self.render('new_post.html', subject='', content='')
+        self.render_new_post()
 
     def post(self):
+        # Retrieve entries
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
         # Check entries
+        if subject and content:
+            # Success case
 
-        # Populate DB
+            # Populate DB
+            e = BlogEntry(subject=subject, content=content)
+            e.put()
 
-        # Render new URL with blog
-        pass
+            # Render new URL with blog
+            self.redirect('/blog')
+
+        else:
+            # Error case
+            # Render new post URL with error
+            self.render_new_post(subject,
+                                 content,
+                                 'Please enter subject and content')
+
 
 app = webapp2.WSGIApplication([
     ('/blog', BlogFrontPage),
